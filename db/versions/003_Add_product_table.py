64424e9c3f46763afaa3d790071602c62c837c61
Risyad Rais <satmo54@gmail.com>
2021-07-31 15:33:03 +0700

    Tambah tabel produk

--- db/versions/003_Add_product_table.py
+++ db/versions/003_Add_product_table.py
@@ -1,13 +1,25 @@
-from sqlalchemy import *
-from migrate import *
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
+meta = MetaData()
+
+product = Table(
+        'product', meta,
+        Column('id', Integer, primary_key=True),
+        Column('nama', String(40)),
+        Column('kategori', Integer),
+        Column('stok', Integer),
+        )
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    pass
+    meta.bind = migrate_engine
+    product.create()
+    # pass
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    pass
+    # pass
+    meta.bind = migrate_engine()
+    product.drop()
